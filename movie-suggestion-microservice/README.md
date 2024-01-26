3ff9d6c2a5984ddeb66e81d403de04123cf4cb08
Abdifatah Mohamed <mofaatah@gmail.com>
2024-01-26 02:16:36 -0600

    Added gitignore & microservice API

---
+++ movie-suggestion-microservice/README.md
@@ -0,0 +1,12 @@
+# Movie WatchList Microservice
+
+Welcome to the Movie WatchList microservice – your dedicated backend API for the Movie WatchList web app, seamlessly hosted on [Render.com](https://movie-suggestion-microservice.onrender.com)
+.
+
+## About
+
+This microservice serves as the backbone for the Movie WatchList web app, providing robust backend functionality. It functions as a sophisticated recommendation engine, offering tailored movie suggestions based on user preferences.
+
+## API Endpoint
+
+Explore the Movie WatchList API at [Movie Suggestion Microservice API](https://movie-suggestion-microservice.onrender.com). Tailor your movie preferences and unlock a world of curated cinematic recommendations. 🎥🍿
---
+++ movie-suggestion-microservice/server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+const moviesData = fs.readFileSync('movies.json');
+const movies = JSON.parse(moviesData);
+
+// Welcome route for the root URL
+app.get('/', (req, res) => {
+    res.send('Welcome to the Movie Suggestion Microservice!');
+});
+
+app.get('/movies', (req, res) => {
+    res.json(movies);
+});
+
+app.get('/movies/:id', (req, res) => {
+    const movieId = parseInt(req.params.id);
+    const movie = movies.find(movie => movie.id === movieId);
+
+    if (movie) {
+        res.json(movie);
+    } else {
+        res.status(404).json({ error: 'Movie not found' });
+    }
+});
+
+// New endpoint for searching movies by genre
+app.get('/search', (req, res) => {
+    const genreQuery = req.query.genre;
+
+    if (!genreQuery) {
+        res.status(400).json({ error: 'Genre parameter is required' });
+        return;
+    }
+
+    const matchingMovies = movies.filter(movie => movie.genre.toLowerCase() === genreQuery.toLowerCase());
+
+    if (matchingMovies.length > 0) {
+        res.json(matchingMovies);
+    } else {
+        res.status(404).json({ error: 'No movies found for the specified genre' });
+    }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
