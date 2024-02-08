21c5e47820e089ef128f29cf0bf37152766a8c63
Abdifatah Mohamed <mofaatah@gmail.com>
2024-02-07 18:17:10 -0600

    Added movie watchlist app UI with react and added cors to the api movie

--- movie-suggestion-microservice/server.js
+++ movie-suggestion-microservice/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const cors = require('cors');
+app.use(cors());
 
 const moviesData = fs.readFileSync('movies.json');
 const movies = JSON.parse(moviesData);
